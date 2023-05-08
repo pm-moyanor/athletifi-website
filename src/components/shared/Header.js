d0f2c836f01a625078c98e27f9dfd10391b89b55
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2023-05-07 20:12:24 -0400

    refactored menubar/header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,29 +1,51 @@
-import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Collapse, NavbarToggler, } from 'reactstrap';
 // import '../styles/header.scss';
 
+const BsNavLink = props => {
+  const { href, title } = props;
+  return (
+    <NavLink href={href} className="nav-link port-navbar-link">{title}</NavLink>
+    )
+  }
+  
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <Navbar color="dark" dark expand="md">
-        <NavbarBrand href="/">AthletiFi</NavbarBrand>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="/">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/about">About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/marketplace">Marketplace</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/mint">Mint</NavLink>
-          </NavItem>
-        </Nav>
+      <Navbar
+        className="port-navbar port-default absolute"
+        color="transparent"
+        dark
+        expand="md"
+      >
+        <NavbarBrand className="port-navbar-brand" href="/">AthletiFi</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/" title="Home"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/about" title="About"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/marketplace" title="Marketplace"/>
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/mint" title="Mint"/>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     </>
   )
 }
 
 export default Header;
+
+
