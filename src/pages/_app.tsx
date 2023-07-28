d07926d3e729cb24abc392eb969ad3e3f19d742b
vikram <vikramjakhal577@gmail.com>
2023-07-28 17:12:46 +0530

    add-aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+    setInterval(() => {
+      AOS.init({
+        once: true,
+      });
+    }, 1000);
+  }, [AOS]);
   return <Component {...pageProps} />;
 }
