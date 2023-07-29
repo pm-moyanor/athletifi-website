79e2a52e87925cbe4a33c13a2df583ce7b296c7b
Deepanshu <deepanshujangra33233@.com>
2023-07-29 16:40:37 +0530

    add seo

--- src/pages/news-insight.tsx
+++ src/pages/news-insight.tsx
@@ -9,26 +9,25 @@ import RecentNewsInsights from "../../components/news-insights/RecentNewsInsight
 const newsinsight = () => {
   const hero = {
     heading: "News and Insights",
-         title: "Your Title Here", // Add the 'title' property
-    subtitle: "Your Subtitle Here", 
+    title: "Your Title Here", // Add the 'title' property
+    subtitle: "Your Subtitle Here",
   };
   const pageSEO = {
     // SEO TITLE
     title: "News and Insights",
 
     // SEO DESCRIPTION
-    description:
-      "Unlocking opportunities for aspiring young athletes. Connecting all talent, no matter where they are from, with top-tier coaches, scholarships, and unparalledled resources. Welcome to the future of sports... for everyone!",
+    description: "news_meta_img",
 
     // SEO WEBSITE URL
     websiteURL: "https://athletifi-web.vercel.app/news-insight",
 
     // SEO IMAGE
-    image: "/lending_meta_img.webp",
+    image: "/news_meta_img.png",
   };
   return (
     <>
-    <Seo pageSEO={pageSEO} />
+      <Seo pageSEO={pageSEO} />
       <div className="overflow_hidden">
         <div className="bg-new-hero-img bg-center bg-no-repeat bg-cover">
           <Header />
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -4,12 +4,11 @@ import Header from "../../components/common/Header";
 import Seo from "../../components/home/Seo";
 import SignUpForm from "../../components/sign-up/SignUpForm";
 
-
 const signup = () => {
   const hero = {
     heading: "Sign Up for Exclusive Access!",
-         title: "Your Title Here", // Add the 'title' property
-    subtitle: "Your Subtitle Here", 
+    title: "Your Title Here", // Add the 'title' property
+    subtitle: "Your Subtitle Here",
   };
   const pageSEO = {
     // SEO TITLE
@@ -17,17 +16,17 @@ const signup = () => {
 
     // SEO DESCRIPTION
     description:
-      "Unlocking opportunities for aspiring young athletes. Connecting all talent, no matter where they are from, with top-tier coaches, scholarships, and unparalledled resources. Welcome to the future of sports... for everyone!",
+      "Signup for exclusive updates! Become part of the sport's revolution.",
 
     // SEO WEBSITE URL
     websiteURL: "https://athletifi-web.vercel.app/news-insight",
 
     // SEO IMAGE
-    image: "/lending_meta_img.webp",
+    image: "/sign_up_meta",
   };
   return (
     <>
-     <Seo pageSEO={pageSEO} />
+      <Seo pageSEO={pageSEO} />
       <div className="bg-signup-hero-img bg-no-repeat bg-cover overflow-hidden">
         <Header />
         <CommonHero hero={hero} />
