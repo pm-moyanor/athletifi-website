05f5099c42259e2b93a20cd5dcd67dea90841827
Deepanshu <deepanshujangra33233@.com>
2023-07-31 21:10:50 +0530

    add expensive-football-img

--- components/home/UniqueAthletifi.tsx
+++ components/home/UniqueAthletifi.tsx
@@ -5,16 +5,15 @@ const UniqueAthletifi = () => {
   return (
     <div>
       <section className="relative lg:before:content-[''] before:absolute lg:before:w-[448px] before:h-[448px] before:-top-20 before:left-0 before:bg-shadow_blue before:blur-[111px] before:opacity-25 before:-translate-x-1/3 before:-z-10 before:rounded-full after:content-[''] after:absolute after:w-[448px] after:h-[448px] after:-bottom-20 after:right-0 after:bg-shadow_blue after:blur-[111px] after:opacity-25 after:translate-x-1/3 after:-z-10 after:rounded-full lg:pb-24 sm:pb-12">
-        <div className="container md:max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto pt-14 relative z-10 lg:mt-14 xl:mt-0">
-          <div 
-          className="lg:px-14 lg:pt-14 sm:px-8 px-4 pt-6 sm:bg-playground-unique bg-playground-unique-sm bg-opacity-50 bg-[length:100%_100%] bg-center bg-no-repeat flex flex-col items-center ">
+        <div className="container md:max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto lg:pt-14 relative z-10 lg:mt-14 xl:mt-0">
+          <div className="lg:px-14 lg:pt-14 sm:px-8 px-4 pt-6 sm:bg-playground-unique bg-playground-unique-sm bg-opacity-50 bg-[length:100%_100%] bg-center bg-no-repeat flex flex-col items-center ">
             <h2
               className="lg:text-5xl md:text-[40px] sm:text-4xl text-[26px] text-primary font-HelveticaNeueMedium text-center py-4 "
-            data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200"
+              data-aos="fade-up"
+              data-aos-duration="600"
+              data-aos-easing="linear"
+              data-aos-delay="200"
+              data-aos-offset="200"
             >
               <span className="relative after:content after:absolute after:bottom-[-7px] sm:after:bottom-[-2px] after:left-0 md:after:w-[209px] after:w-[150px] after:h-2 after:bg-blue-underline after:bg-contain after:bg-no-repeat mb-4 lg:leading-[60px] leading-[120%]">
                 What Makes
@@ -22,28 +21,28 @@ const UniqueAthletifi = () => {
               AthletiFi Unique
             </h2>
             <article
-           data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200"><p
-              className="font-Segoe text-md opacity-80 text-primary text-center md:pb-10 pb-5"
-              
+              data-aos="fade-up"
+              data-aos-duration="600"
+              data-aos-easing="linear"
+              data-aos-delay="200"
+              data-aos-offset="200"
             >
-              Stats allow friends, family, and fans to easily support and follow
-              their favorite players or players in their community{" "}
-              <span className="lg:block">
-                On the other hand, stats allow coaches and scouts to make
-                data-driven choices when building their roster and in-game
-                strategy
-              </span>
-            </p></article>
+              <p className="font-Segoe text-md opacity-80 text-primary text-center md:pb-10 pb-5">
+                Stats allow friends, family, and fans to easily support and
+                follow their favorite players or players in their community{" "}
+                <span className="lg:block">
+                  On the other hand, stats allow coaches and scouts to make
+                  data-driven choices when building their roster and in-game
+                  strategy
+                </span>
+              </p>
+            </article>
             <Image
-           data-aos="fade-in"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200"
+              data-aos="fade-in"
+              data-aos-duration="600"
+              data-aos-easing="linear"
+              data-aos-delay="200"
+              data-aos-offset="200"
               className="mb-[1px] lg:max-w-[267px] max-w-[180px] sm:max-w-[220px]"
               src="/assets/img/png/portrait-young.png"
               width={267}
