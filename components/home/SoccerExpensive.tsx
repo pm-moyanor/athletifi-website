59fb653533998c68e61d49e9c48d9e2476c8e99e
Sanjay2929 <sanjayjyani220@gmail.com>
2023-07-31 17:48:51 +0530

    areal label added

--- components/home/SoccerExpensive.tsx
+++ components/home/SoccerExpensive.tsx
@@ -19,35 +19,28 @@ const SoccerExpensive = () => {
         <div className="container md:max-w-full xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto">
           <div className="max-w-[623px] mx-auto lg:mx-0 relative after:content after:absolute lg:after:w-[690px] lg:after:h-[420px] lg:after:bg-[#040F16] after:blur-[55px] after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:z-0">
             <div
-             data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200" className=" relative z-10">
-              <h2
-                className="font-HelveticaNeueMedium text-primary xl:text-xl lg:text-[43px] md:text-[40px] font-medium mb-4 text-[25px] sm:text-[35px] text-center lg:text-start leading-[120%] xl:leading-[60px]"
-            
-              >
+              data-aos="fade-up"
+              data-aos-duration="600"
+              data-aos-easing="linear"
+              data-aos-delay="200"
+              data-aos-offset="200"
+              className=" relative z-10"
+            >
+              <h2 className="font-HelveticaNeueMedium text-primary xl:text-xl lg:text-[43px] md:text-[40px] font-medium mb-4 text-[25px] sm:text-[35px] text-center lg:text-start leading-[120%] xl:leading-[60px]">
                 <span className="relative after:content after:absolute after:bottom-[-4px] sm:after:bottom-[-2px] after:left-0 md:after:w-[220px] after:w-[120px] after:h-2 after:bg-blue-underline after:bg-contain after:bg-no-repeat">
                   Soccer is{" "}
                 </span>
                 Expensive & Reduces Accessibility
               </h2>
-              <p
-                className="font-Segoe font-normal text-md text-primary opacity-80 text-center lg:text-start"
-               
-              >
+              <p className="font-Segoe font-normal text-md text-primary opacity-80 text-center lg:text-start">
                 There is an unfortunate contradiction at the heart of club
                 soccer — a sport renowned for its global unity, yet hindered by
                 prohibitive costs that reduce accessibility and limit
                 participation. Club soccer is creating a system that excludes
                 potential players from less privileged backgrounds.
               </p>
-              <div
-                className="flex lg:justify-start justify-center items-center mt-10 lg:mb-10"
-              
-              >
-                <Link href="/about-us">
+              <div className="flex lg:justify-start justify-center items-center mt-10 lg:mb-10">
+                <Link aria-label="About us page" href="/about-us">
                   <BlueButton text="Read more" onClick={() => {}} />
                 </Link>
               </div>
@@ -55,11 +48,11 @@ const SoccerExpensive = () => {
           </div>
         </div>
         <div
-        data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200"
+          data-aos="fade-up"
+          data-aos-duration="600"
+          data-aos-easing="linear"
+          data-aos-delay="200"
+          data-aos-offset="200"
         >
           <div className="absolute right-0 lg:top-[40%] lg:-translate-y-1/2 xl:w-[700px] xl:h-[1000px] w-[300px] sm:w-[450px] lg:w-[500px] hidden lg:inline-block">
             <Image
@@ -72,11 +65,11 @@ const SoccerExpensive = () => {
           </div>
         </div>
         <div
-         data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-easing="linear"
-            data-aos-delay="200"
-            data-aos-offset="200"
+          data-aos="fade-up"
+          data-aos-duration="600"
+          data-aos-easing="linear"
+          data-aos-delay="200"
+          data-aos-offset="200"
         >
           <div className="lg:hidden relative w-full sm:w-[60%] mx-auto mt-5">
             <Image
