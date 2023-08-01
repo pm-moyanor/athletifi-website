c3c6140ef1b61fd2eac40c8d753df6155a4fb7a0
Deepanshu <deepanshujangra33233@.com>
2023-08-01 12:26:59 +0530

    add aria-lebel btn

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -10,8 +10,8 @@ import Seo from "../../components/home/Seo";
 const aboutUs = () => {
   const hero = {
     heading: "About us",
-     title: "Your Title Here", // Add the 'title' property
-    subtitle: "Your Subtitle Here", 
+    title: "Your Title Here", // Add the 'title' property
+    subtitle: "Your Subtitle Here",
   };
   const pageSEO = {
     // SEO TITLE
