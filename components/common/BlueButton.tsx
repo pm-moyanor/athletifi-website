9f40f0987e144e5e6f6913ac52cf13209fdb63b3
Sanjay2929 <sanjayjyani220@gmail.com>
2023-08-04 12:12:09 +0530

    button cursior pointer

--- components/common/BlueButton.tsx
+++ components/common/BlueButton.tsx
@@ -13,7 +13,7 @@ const BlueButton: React.FC<BlueButtonProps> = (props) => {
   return (
     <>
       <span
-        className="sm:px-[24px] px-4 sm:py-[14.5px] py-2 flex bg-skyblue text-base font-semibold text-black font-Segoe leading-6 gap-[6px] group border border-skyblue hover:bg-black hover:text-skyblue join_now_btn transition duration-300 ease-in-out"
+        className="sm:px-[24px] px-4 sm:py-[14.5px] py-2 flex bg-skyblue text-base font-semibold text-black font-Segoe leading-6 gap-[6px] group border border-skyblue hover:bg-black hover:text-skyblue join_now_btn transition duration-300 ease-in-out cursor-pointer"
         onClick={props.onClick}
       >
         {props.text}
