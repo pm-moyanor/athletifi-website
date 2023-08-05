d5be08168de312739312b879d17e507f0433e7cb
Deepanshu <deepanshujangra33233@.com>
2023-08-05 17:37:47 +0530

    footer icon change

--- components/common/SocialPopUp.tsx
+++ components/common/SocialPopUp.tsx
@@ -12,17 +12,8 @@ import {
 const SocialPopUp = () => {
   return (
     <>
-      <div className="bg_social_icon flex items-start justify-center gap-[20px] sm:fixed sm:top-[90px] top-[100%] left-[20%] sm:left-1/2 z-20 flex-col-reverse p-6">
+      <div className="bg_social_icon flex items-start justify-center gap-[20px] sm:fixed sm:top-[90px] top-[100%] left-[20%] sm:left-1/2 z-20 flex-col-reverse p-6 ">
         {/* CONTACT US BUTTON */}
-        <Link
-          href="/sign-up"
-          className="sm:px-[24px] px-4 sm:py-[14.5px] py-2 flex bg-skyblue text-base font-semibold text-white font-Segoe leading-6 gap-[6px] group border border-skyblue hover:bg-black hover:text-skyblue join_now_btn transition duration-300 ease-in-out social_btn_contact_us"
-        >
-          Contact Us
-          <span className="group-hover:translate-x-3 transition duration-300 ease-out">
-            <ArrowButton />
-          </span>
-        </Link>
         <div className="flex gap-4 flex-col">
           {/* SOCIAL ICONS LINKS */}
           <Link
@@ -83,6 +74,15 @@ const SocialPopUp = () => {
             <LinkedInIcon />
             <span className="text-white opacity-70 ms-4 text-md font-Segoe font-normal">
               Linkedin
+            </span>
+          </Link>
+          <Link
+            href="/sign-up"
+            className="sm:px-[24px] px-4 sm:py-[14.5px] py-2 flex bg-skyblue text-base font-semibold text-white font-Segoe leading-6 gap-[6px] group border border-skyblue hover:bg-black hover:text-skyblue join_now_btn transition duration-300 ease-in-out social_btn_contact_us relative z-20"
+          >
+            Contact Us
+            <span className="group-hover:translate-x-3 transition duration-300 ease-out">
+              <ArrowButton />
             </span>
           </Link>
         </div>
