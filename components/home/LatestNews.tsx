0dc41e59d1c3b1a1e7a1bfa056e47a99bc58e191
muskan <muskansaini758@gmail.com>
2023-09-19 12:15:49 +0530

    add pagination

--- components/home/LatestNews.tsx
+++ components/home/LatestNews.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import BlueButton from "../common/BlueButton";
 
 interface NewsProps {
@@ -9,7 +9,22 @@ interface NewsProps {
 
 const LatestNews: React.FC<NewsProps> = (props) => {
   const { allNewsList } = props;
-console.log("allNewsListallNewsList",allNewsList)
+  const DataArray=allNewsList.data
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(
+    allNewsList.meta.pagination.pageCount
+  );
+
+  console.log("currentPagecurrentPagecurrentPage", allNewsList);
+  // Calculate the start and end indexes of the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedItems = DataArray.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(DataArray.length / itemsPerPage);
+
+  const handlePageChange = (newPage:any) => {
+    setCurrentPage(newPage);
+  };
   return (
     <>
       <div className="py-14 md:py-0 relative before:content-[''] before:absolute sm:before:w-[448px] before:w-[248px] sm:before:h-[448px] before:h-[248px] before:top-0 before:left-0 before:bg-shadow_blue before:blur-[111px] before:opacity-25 before:-translate-x-1/4 before:z-0 before:rounded-full after:content-[''] after:absolute sm:after:w-[448px] sm:after:h-[448px] after:w-[248px] after:h-[248px] after:bottom-20 after:right-0 after:bg-shadow_blue after:blur-[111px] after:opacity-25 after:translate-x-1/4 after:z-0 after:rounded-full">
@@ -20,62 +35,79 @@ console.log("allNewsListallNewsList",allNewsList)
             data-aos-easing="linear"
             data-aos-delay="100"
             data-aos-offset="200"
-            className="text-center font-HelveticaNeueMedium font-medium lg:text-5xl md:text-[40px] text-[26px] text-primary md:py-10 pb-5">
+            className="text-center font-HelveticaNeueMedium font-medium lg:text-5xl md:text-[40px] text-[26px] text-primary md:py-10 pb-5"
+          >
             <span className="relative after:content after:absolute after:bottom-[-6px] md:after:bottom-[-2px] after:left-0 md:after:w-[209px] after:w-[150px] after:h-2 after:bg-blue-underline after:bg-contain after:bg-no-repeat">
               Latest News
             </span>
           </h2>
-          {allNewsList.data.map((val: any, index: any) => {
+          {displayedItems.map((val: any, index: any) => {
             return (
-             <Link href={`/news/${val.slug}`}>
-              <div
-                className="flex md:flex-row flex-col lg:p-8 p-6 bg-darkgray lg:gap-x-12 sm:gap-7 gap-3 rounded-[20px] mb-6 sm:mt-5  "
-                data-aos="fade-up"
-                data-aos-duration="400"
-                data-aos-easing="ease"
-                data-aos-delay={val.delay}
-                key={index}>
-                <Image
-                  className="md:w-[315px] rounded-[10px] object-cover w-full"
-                  src={`https://vidalco.in${val.image.url}`}
-                  width={315}
-                  height={240}
-                  alt="football match"
-                />
-                <div className="sm:pt-0 pt-3">
-                  {/* NEWS HEADING */}
-                  <h3 className="sm:text-basemd text-[18px] text-primary font-HelveticaNeueMedium leading-[140%]">
-                    {val.title}{" "}
-                  </h3>
-                  <div className="flex sm:items-center sm:flex-row flex-col sm:gap-3 gap-2 pt-2">
-                    <h4 className="lg:pe-3 ">
-                      <span className="lg:text-md text-base text-primary font-Segoe opacity-80 font-normal lg:pe-2 pe-1">
-                        by
-                      </span>{" "}
-                      <span className="lg:text-base text-sm text-primary font-Segoe font-semibold">
-                        {val.author.fullName}
-                      </span>
-                    </h4>
-                    {/* NEWS CATEGORY */}
-                    {val.categories.map((obj: any, i: any) => {
-                      return (
-                        <span key={i} className="flex gap-x-3">
-                          <button className="lg:text-base md:text-[13px] text-sm text-skyblue font-Segoe font-normal py-2 px-3 sm:py-[10px] sm:px-[18px] bg-matchtittles rounded-full leading-[150%] duration-300 hover:text-white">
-                            {obj.title}
-                          </button>
+              <Link href={`/news/${val.slug}`}>
+                <div
+                  className="flex md:flex-row flex-col lg:p-8 p-6 bg-darkgray lg:gap-x-12 sm:gap-7 gap-3 rounded-[20px] mb-6 sm:mt-5  "
+                  data-aos="fade-up"
+                  data-aos-duration="400"
+                  data-aos-easing="ease"
+                  data-aos-delay={val.delay}
+                  key={index}
+                >
+                  <Image
+                    className="md:w-[315px] rounded-[10px] object-cover w-full"
+                    src={`https://vidalco.in${val.image.url}`}
+                    width={315}
+                    height={240}
+                    alt="football match"
+                  />
+                  <div className="sm:pt-0 pt-3">
+                    {/* NEWS HEADING */}
+                    <h3 className="sm:text-basemd text-[18px] text-primary font-HelveticaNeueMedium leading-[140%]">
+                      {val.title}{" "}
+                    </h3>
+                    <div className="flex sm:items-center sm:flex-row flex-col sm:gap-3 gap-2 pt-2">
+                      <h4 className="lg:pe-3 ">
+                        <span className="lg:text-md text-base text-primary font-Segoe opacity-80 font-normal lg:pe-2 pe-1">
+                          by
+                        </span>{" "}
+                        <span className="lg:text-base text-sm text-primary font-Segoe font-semibold">
+                          {val.author.fullName}
                         </span>
-                      );
-                    })}
+                      </h4>
+                      {/* NEWS CATEGORY */}
+                      {val.categories.map((obj: any, i: any) => {
+                        return (
+                          <span key={i} className="flex gap-x-3">
+                            <button className="lg:text-base md:text-[13px] text-sm text-skyblue font-Segoe font-normal py-2 px-3 sm:py-[10px] sm:px-[18px] bg-matchtittles rounded-full leading-[150%] duration-300 hover:text-white">
+                              {obj.title}
+                            </button>
+                          </span>
+                        );
+                      })}
+                    </div>
+                    {/* NEWS DATA */}
+                    <p className="sm:text-md text:base text-primary opacity-80 font-Segoe leading-[150%] sm:pt-6 pt-3 lg:pe-14 sm:pe-4">
+                      {val.description}
+                    </p>
                   </div>
-                  {/* NEWS DATA */}
-                  <p className="sm:text-md text:base text-primary opacity-80 font-Segoe leading-[150%] sm:pt-6 pt-3 lg:pe-14 sm:pe-4">
-                    {val.description}
-                  </p>
                 </div>
-              </div>
-             </Link>
+              </Link>
             );
           })}
+           <div className="pagination">
+        <button className="text-white"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button className="text-white"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+       
+      </div>
           <div className="flex justify-center items-center pt-4 md:pb-14 lg:mb-10">
             <Link href="/news">
               <BlueButton text="View all" />
