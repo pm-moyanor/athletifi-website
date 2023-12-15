cbc256782c659059e6361c9eb290542828f36b12
Hector Agudelo <haacny86@gmail.com>
2023-12-15 14:32:34 -0500

    Player info title font size change to a smaller font

--- src/components/dashboard/PlayerInfo.tsx
+++ src/components/dashboard/PlayerInfo.tsx
@@ -20,7 +20,7 @@ const PlayerInfo = () => {
                     data-aos-delay="300"
                     data-aos-offset="100"
                 >
-                    <h2 className="font-HelveticaNeueMedium md:text-5xl text-[26px] sm:text-4xl text-[#FDFEFF] font-medium leading-[60px] relative z-20 text-center md:mb-4">
+                    <h2 className="font-HelveticaNeueMedium md:text-4xl text-[26px] text-[#FDFEFF] font-medium leading-[60px] relative z-20 text-center md:mb-4">
                         <span className="relative ">
                             Player Info
                             <span className="absolute -bottom-2 left-0 z-0">
