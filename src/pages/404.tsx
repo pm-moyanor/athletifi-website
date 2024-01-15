9b1c9934dc7459eda6210169dfedc9053d7fcc5f
louis.code <louis.code88@gmail.com>
2024-01-15 13:18:07 -0700

    feat: add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import CommonHero from "@/components/common/CommonHero";
+import Custom404 from "@/components/common/Custom404";
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import Seo from "@/components/common/Seo";
+import { SEO_CONFIG } from "@/utils/seoConfig";
+
+const NotFound = () => {
+  const hero = {
+    heading: "",
+  };
+
+  return (
+    <>
+      {/* SEO */}
+      <Seo pageSEO={SEO_CONFIG.signUp} />
+      <div>
+        <Header />
+        <CommonHero hero={hero} />
+      </div>
+      <Custom404 />
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
