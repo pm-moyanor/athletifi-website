5e6c001c53bb5c70474f269b5f36cd18b8deb001
louis.code <louis.code88@gmail.com>
2024-02-09 20:12:05 -0700

    feat: add contactUs button in footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -10,6 +10,7 @@ import {
   TikTokIcon,
   TwitterIcon,
   PageLogo,
+  ArrowButton,
 } from './Icon';
 import Link from 'next/link';
 
@@ -20,14 +21,13 @@ const Footer = () => {
   const path: string = useRouter().pathname;
   return (
     <>
-      <footer className='flex flex-col border-t border-darkerSkyBlue h-541'>
-        <div className='container flex flex-col items-start gap-5 md:flex-row md:justify-between md:max-w-full xl:max-w-1140 2xl:max-w-1320 mx-auto my-32'>
-          <div className='flex flex-col justify-start'>
+      <footer className="flex flex-col border-t border-darkerSkyBlue h-541">
+        <div className="container flex flex-col items-start gap-5 md:flex-row md:justify-between md:max-w-full xl:max-w-1140 2xl:max-w-1320 mx-auto my-32">
+          <div className="flex flex-col justify-start">
             <ul>
               <li>
                 <Link
-           
-                  href='/'
+                  href="/"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/' ? '!opacity-100' : ''
                   }`}
@@ -37,8 +37,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link
-             
-                  href='/about-us'
+                  href="/about-us"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/about-us' ? '!opacity-100 ' : ''
                   }`}
@@ -48,8 +47,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link
-            
-                  href='/news?page=1'
+                  href="/news?page=1"
                   className={`text-md text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/news' ? '!opacity-100' : ''
                   }`}
@@ -59,8 +57,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link
-           
-                  href='/contact-us'
+                  href="/contact-us"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/contact-us' ? '!opacity-100' : ''
                   }`}
@@ -83,14 +80,13 @@ const Footer = () => {
                 </li> */}
             </ul>
           </div>
-          <div className='flex flex-col justify-start'>
+          <div className="flex flex-col justify-start">
             <ul>
               <li>
                 <Link
-              
-                  href='https://www.athletifiselect.com/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.athletifiselect.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto
               
                   }`}
@@ -100,8 +96,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link
-           
-                  href='/privacy-policy'
+                  href="/privacy-policy"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/privacy-policy' ? '!opacity-100' : ''
                   }`}
@@ -111,8 +106,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link
-        
-                  href='/terms-of-use'
+                  href="/terms-of-use"
                   className={`text-base text-white font-normal font-Segoe opacity-70 hover:opacity-100 duration-300 relative after:content-[''] after:absolute after:w-0 hover:after:w-full after:h-2pixel after:-bottom-1 after:right-0 after:bg-shadow_blue after:rounded-md after:transition-all after:duration-300 after:ease-out hover:after:left-0 hover:after:right-auto ${
                     path == '/terms-of-use' ? '!opacity-100' : ''
                   }`}
@@ -122,63 +116,73 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div></div>
+          <div>
+            <Link
+              href="/contact-us"
+              className="sm:px-24pixel px-4 sm:py-14.5 py-2 flex bg-skyblue text-base font-semibold text-white font-Segoe leading-6 gap-6pixel group border border-skyblue hover:bg-black hover:text-skyblue btn__cta transition duration-300 ease-in-out social-popup__btn"
+            >
+              Contact Us
+              <span className="group-hover:translate-x-3 transition duration-300 ease-out">
+                <ArrowButton />
+              </span>
+            </Link>
+          </div>
         </div>
 
-        <div className='container md:max-w-full xl:max-w-1140 2xl:max-w-1320 mx-auto  flex flex-col md:flex-row gap-5 items-center justify-between py-7 border-t border-extraDarkBlue'>
+        <div className="container md:max-w-full xl:max-w-1140 2xl:max-w-1320 mx-auto  flex flex-col md:flex-row gap-5 items-center justify-between py-7 border-t border-extraDarkBlue">
           <div>
-            <Link href='/'>
+            <Link href="/">
               <PageLogo />
             </Link>
           </div>
-          <p className='text-white text-sm font-Segoe opacity-70'>
+          <p className="text-white text-sm font-Segoe opacity-70">
             @{year} Athletifi. All rights reserved
           </p>
 
           {/* SOCIAL-ICON */}
-          <div className='flex gap-4'>
+          <div className="flex gap-4">
             <Link
-              aria-label='Tik-Tok'
-              className='hover:-translate-y-1 transition duration-300 ease-out'
-              href='https://www.tiktok.com/@athletifi'
-              target='_blank'
-              rel='noopener noreferrer'
+              aria-label="Tik-Tok"
+              className="hover:-translate-y-1 transition duration-300 ease-out"
+              href="https://www.tiktok.com/@athletifi"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <TikTokIcon />
             </Link>
             <Link
-              aria-label='facebook'
-              className='hover:-translate-y-1 transition duration-300 ease-out'
-              href='https://www.facebook.com/profile.php?id=61553263775533'
-              target='_blank'
-              rel='noopener noreferrer'
+              aria-label="facebook"
+              className="hover:-translate-y-1 transition duration-300 ease-out"
+              href="https://www.facebook.com/profile.php?id=61553263775533"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <FacebookIcon />
             </Link>
             <Link
-              aria-label='twitter'
-              className='hover:-translate-y-1 transition duration-300 ease-out'
-              href='https://twitter.com/Athletifi'
-              target='_blank'
-              rel='noopener noreferrer'
+              aria-label="twitter"
+              className="hover:-translate-y-1 transition duration-300 ease-out"
+              href="https://twitter.com/Athletifi"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <TwitterIcon />
             </Link>
             <Link
-              aria-label='instagram'
-              className='hover:-translate-y-1 transition duration-300 ease-out'
-              href='https://www.instagram.com/athletifi/'
-              target='_blank'
-              rel='noopener noreferrer'
+              aria-label="instagram"
+              className="hover:-translate-y-1 transition duration-300 ease-out"
+              href="https://www.instagram.com/athletifi/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <InstaIcon />
             </Link>
             <Link
-              aria-label='linkedin'
-              className='hover:-translate-y-1 transition duration-300 ease-out'
-              href='https://www.linkedin.com/company/athletifi/'
-              target='_blank'
-              rel='noopener noreferrer'
+              aria-label="linkedin"
+              className="hover:-translate-y-1 transition duration-300 ease-out"
+              href="https://www.linkedin.com/company/athletifi/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <LinkedInIcon />
             </Link>
