908beba93f09c55f6ef7eb05c1d422ab67a38548
Louis <louis.code88@gmail.com>
2024-02-20 17:13:33 -0700

    fix: rename playerId to cardId for dynamic urls

--- src/pages/dashboard/vsa-23/[cardId].tsx
+++ src/pages/dashboard/vsa-23/[cardId].tsx
@@ -12,8 +12,10 @@ import HeroBanner from '@/components/dashboard/HeroBanner';
 // import PlayerStats from '@/components/dashboard/PlayerStats';
 // import PlayerInfo from '@/components/dashboard/PlayerInfo';
 // import PlayerCard from '@/components/dashboard/PlayerCard';
+import SimpleBarChart from '@/components/dashboard/BarChart';
+import LineExample from '@/components/dashboard/LineChart';
 
-const PlayerDashboardPage = ({ playerId }: PlayerDashboardProps) => {
+const PlayerDashboardPage = ({ cardId }: PlayerDashboardProps) => {
   // SAMPLE DATA
   // TODO: FETCH PLAYER DATA FROM BACKEND
   const playerProfile = {
@@ -37,15 +39,11 @@ const PlayerDashboardPage = ({ playerId }: PlayerDashboardProps) => {
           <CommonHero hero={hero} />
         </div>
         <main className="flex flex-col px-3 min-h-full gap-5 m-10 sm:max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl  mx-auto">
-          <HeroBanner />
-          {/* <section className="flex flex-col justify-center items-stretch lg:flex-row h-full gap-5">
-            <PlayerStats />
-            <PlayerCard />
-            <PlayerInfo />
-          </section> */}
+          <section className="flex flex-col justify-center items-stretch lg:flex-row h-full gap-5">
+            <SimpleBarChart />
+          </section>
           <section className="flex flex-col xl:flex-row justify-center items-stretch flex-grow h-full gap-5 ">
-            <Teammates />
-            <Highlights />
+            <LineExample />
           </section>
         </main>
         <Footer />
