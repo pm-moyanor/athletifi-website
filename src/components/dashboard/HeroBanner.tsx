b0da052061aca9d7ed123b9d7e1ddf43aa6063c1
Hector Agudelo <haacny86@gmail.com>
2024-02-23 12:54:36 -0500

    fixing spaces in components and deleting unnecesary css attributes

--- src/components/dashboard/HeroBanner.tsx
+++ src/components/dashboard/HeroBanner.tsx
@@ -22,50 +22,41 @@ const CARD_IMAGE_HEIGHT: number = 300;
 const HeroBanner: React.FC = () => {
   return (
     <>
-      <section className='flex flex-col justify-center items-stretch lg:flex-row h-full gap-5'>
-        <div className='lg:w-3/4 xl:w-1/2 2xl:w-1/2 min-h-full'>
-          <div className="container__border--blue-gradient bg-blue_linear_gradient after:absolute relative lg:py-16 py-5 md:py-10 z-0  after:contents-[''] after:inset-0 after:p-1 after:rounded-30 rounded-30 h-full flex items-center justify-center">
+      <section className="flex flex-col justify-start items-stretch lg:flex-row h-full pt-5 lg:p-0 z-10">
+        <div className="lg:w-3/4 xl:w-1/2 2xl:w-1/2 min-h-full order-2 lg:order-1">
+          <div className=" relative lg:py-16 py-5 md:py-10 z-0  rounded-30 h-full flex items-center justify-center gap-2">
+            <VillanovaIcon />
             <div
-              className='flex justify-center items-center relative z-20 '
-              data-aos='fade-up'
+              className="flex justify-start items-center relative z-20 "
+              data-aos="fade-up"
               data-aos-duration={AOS_DURATION}
-              data-aos-easing='ease-in-sine'
+              data-aos-easing="ease-in-sine"
               data-aos-delay={AOS_DELAY}
               data-aos-offset={AOS_OFFSET}
             >
-            <VillanovaIcon />
-              <h2 className='font-HelveticaNeueMedium md:text-4xl text-basemd text-primary font-medium leading-60 relative z-20 text-center md:mb-4'>
-                <span className='relative '>
-                  Player Info
-                  <span className='absolute -bottom-2 left-0 z-0'>
-                    <UnderLineText />
-                  </span>
-                </span>
-              </h2>
-              <div className='flex flex-col justify-center p-5'>
-                {Object.entries(playerInformation).map(([key, value]) => (
-                  <div key={key} className='my-2'>
-                    <span className='font-bold font-Segoe text-md leading-7 text-center pb-0.5 text-[#FDFEFF] opacity-80 m-0 sm:pt-4 lg:max-w-[769px] mx-auto sm:px-0.5 px-0.5 xl:px-0.5 relative z-20'>
-                      {key}:{' '}
-                    </span>
-                    <span className='font-Segoe font-normal text-md leading-7 text-center pb-0.5 text-primary opacity-80 m-0 sm:pt-4 lg:max-w-769 mx-auto sm:px-0.5 px-0.5 xl:px-0.5 relative z-20'>
-                      {value}
-                    </span>
-                  </div>
-                ))}
+              <div className="flex flex-col justify-start">
+                <h2 className="font-HelveticaNeueMedium md:text-2xl text-basemd  text-primary font-medium relative z-20 text-start">
+                  {playerInformation.name}
+                </h2>
+                <p className="font-Segoe font-normal text-md leading-7 text-start  text-primary opacity-80 lg:max-w-769 relative z-20">
+                  {playerInformation.club}
+                </p>
+                <p className="font-Segoe font-normal text-md leading-7 text-start  text-primary opacity-80  lg:max-w-769   relative z-20">
+                  {`team ${playerInformation.team}`}
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className='lg:w-full xl:w-1/2 2xl:w-1/2 min-h-full h-full flex justify-center items-center lg:my-5 xl:my-0'>
+        <div className="lg:w-full xl:w-1/2 2xl:w-1/2 min-h-full h-full flex justify-center items-center lg:-mb-56 lg:mt-40 order-1">
           <Image
-            className=''
+            className=""
             src={cardImage}
-            alt='Player card'
+            alt="Player card"
             width={CARD_IMAGE_WIDTH}
             height={CARD_IMAGE_HEIGHT}
             quality={75}
-            loading='lazy'
+            loading="lazy"
           />
         </div>
       </section>
