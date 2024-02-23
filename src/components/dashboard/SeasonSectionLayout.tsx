f988c6e03d214b50b4692a466287f94a18eaabf1
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-02-23 13:37:45 -0500

    refine responsive styling

--- src/components/dashboard/SeasonSectionLayout.tsx
+++ src/components/dashboard/SeasonSectionLayout.tsx
@@ -4,7 +4,7 @@ import { ActionReelList } from './TopActionReels';
 
 const dummyData = [
   {
-    videoSrc: 'video1.mp4',
+    videoSrc: 'video2.mp4',
     title: 'Highlight',
     description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
   },
@@ -23,7 +23,7 @@ const dummyData = [
 const SeasonSection: React.FC = () => {
   return (
     <div className="bg-cardsBackground flex  justify-center items-center">
-      <div className="w-full border max-w-[1000px] sm:py-6 md:py-12 px-4 flex flex-col sm:flex-col md:flex-row justify-center ">
+      <div className="w-full max-w-[1000px] sm:py-6 md:py-16 px-4 flex flex-col sm:flex-col md:flex-row justify-center ">
         <SeasonHighlights />
         <ActionReelList actionReels={dummyData} />
       </div>
--- src/components/dashboard/TopActionReels.tsx
+++ src/components/dashboard/TopActionReels.tsx
@@ -5,7 +5,6 @@ interface ActionReelProps {
   title: string;
   description: string;
 }
-
 const ActionReel: React.FC<ActionReelProps> = ({
   videoSrc,
   title,
@@ -14,8 +13,8 @@ const ActionReel: React.FC<ActionReelProps> = ({
 
 
   return (
-    <div className=" border flex my-2 justify-start">
-      <div className="relative w-full max-w-[240px] min-w-[180px] h-40">
+    <div className=" flex my-2 justify-start">
+      <div className="relative w-full max-w-[240px] min-w-[100px] h-[130px] sm:h-[130px] md:h-[150px]">
         <video className="w-full h-full object-cover rounded rounded-5" src={videoSrc} controls />
         <time className="absolute bottom-2 right-2 bg-black text-primary px-1.5 text-sm font-light">
           00:00
@@ -34,12 +33,13 @@ interface ActionReelListProps {
 }
 
 const ActionReelList: React.FC<ActionReelListProps> = ({ actionReels }) => {
+
   return (
-    <div className="flex flex-col items-center border w-2/3 md:w-1/2">
-      <h3 className="w-full text-primary pt-2 text-md font-semibold font-sourceSansPro my-4">
+    <div className="flex flex-col items-center w-full md:w-1/2">
+      <div className=" flex flex-col md:overflow-auto h-[500px]">
+      <h3 className="w-full text-primary pt-2 text-md font-semibold font-sourceSansPro my-2 md:py-4">
         Top Action Reels
       </h3>
-      <div className=" flex flex-col overflow-auto h-[500px]">
         {actionReels.map((actionReel, index) => (
           <ActionReel key={index} {...actionReel} />
         ))}
