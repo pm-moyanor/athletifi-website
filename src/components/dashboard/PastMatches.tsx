e93adef147f03674e8820511d6541f663ee4b8c2
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-03-11 22:33:42 -0400

    animate past matches list

--- src/components/dashboard/PastMatches.tsx
+++ src/components/dashboard/PastMatches.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MatchSummary from './MatchSummary';
+import { motion } from 'framer-motion';
 
 const PastMatches: React.FC = () => {
   // Dummy match data -- to be defined
@@ -46,22 +47,44 @@ const PastMatches: React.FC = () => {
   //// will depend on how past matches data is collected
   const pastMatchesList = [dummyMatchData, dummyMatchData, dummyMatchData];
 
+  const staggerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.1, // Stagger the children with a delay of 0.5 seconds
+      },
+    },
+  };
+
   return (
     <div className="w-full px-0 md:px-4 lg:px-0 lg:w-2/3 lg:max-w-[640px]">
       <h2 className="text-primary font-semibold text-2xl mb-6 font-sourceSansPro">
         Past matches
       </h2>
-      <div className="flex flex-col">
+      <motion.div
+        variants={staggerVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-col"
+      >
         {pastMatchesList.map((match, index) => (
-          //  replace keys when data is integrated
           <React.Fragment key={index}>
-            <MatchSummary matchData={match} />
+            <motion.div variants={staggerVariants} className="overflow-hidden">
+              <MatchSummary matchData={match} />
+            </motion.div>
             {index !== pastMatchesList.length - 1 && (
-              <span className="h-px my-4 md:my-6 bg-partnersBorders inline-block min-w-min" />
+              <motion.span
+                variants={staggerVariants}
+                initial="hidden"
+                animate="visible"
+                className="h-px my-4 md:my-6 bg-partnersBorders inline-block min-w-min"
+              ></motion.span>
             )}
           </React.Fragment>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
