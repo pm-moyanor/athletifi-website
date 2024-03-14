8cde32a1585a2cb7c1a12f9704335958dac05f27
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-03-14 13:53:04 -0400

    split highlights into string array

--- src/components/dashboard/SeasonHighlights.tsx
+++ src/components/dashboard/SeasonHighlights.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 
+const highlightsList = [
+  'First highlight of the season. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu vehicula odio. Duis feugiat massa at tellus convallis, sit amet tincidunt dui faucibus.',
+  'Second highlight of the season. Fusce quis vehicula leo, ac convallis lorem. Phasellus nec metus nec nunc congue consectetur. Maecenas consequat ligula nec malesuada efficitur.',
+  'Third highlight of the season. Nulla facilisi. Aliquam erat volutpat. Duis nec enim ut mi consequat venenatis. Quisque eget lobortis elit, ac vestibulum enim.',
+  'Fourth highlight of the season. Sed vitae mauris nec lorem imperdiet ultricies. Proin et est ultricies, sollicitudin ex at, tincidunt urna. Vivamus nec lacinia quam, non laoreet orci.',
+];
+
 const SeasonHighlights: React.FC<{ data: string[] | null[] }> = ({
   data,
 }: {
@@ -14,25 +21,18 @@ const SeasonHighlights: React.FC<{ data: string[] | null[] }> = ({
     md:my-0 items-center md:items-start max-h-[460px]"
         >
           <div className="">
-            <h2 className="text-[24px] md:text-lg text-primary font-[600] mb-4 text-top">
+            <h2 className="text-[24px] md:text-lg text-primary font-[600] mb-6 text-top">
               Season Highlights
             </h2>
-            <p className="text-[16px] text-primary font-extralight max-w-[560px] md:max-w-[440px] pr-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Voluptates dolores sunt accusantium officiis, nesciunt nemo
-              voluptatem alias, laboriosam nihil inventore odio dicta tempore
-              tenetur itaque non numquam commodi hic labore.{' '}
-              <span className=" block h-2" /> Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro molestias explicabo nobis
-              incidunt error repudiandae dolor nostrum, dignissimos temporibus
-              delectus quam at sed in cupiditate, harum optio beatae ex veniam?{' '}
-              <span className=" block h-2" />
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse
-              recusandae, commodi eveniet ipsa Nesciunt similique ducimus et?{' '}
-              <span className=" block h-2" /> Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Porro molestias explicabo nobis
-              incidunt error repudiandae dolor nostrum
-            </p>
+            {highlightsList.map((highlight, index) => (
+              <React.Fragment key={index}>
+                <div className="h-px bg-partnersBorders w-full my-4" />
+
+                <p className="text-[16px] text-primary font-extralight max-w-[560px] md:max-w-[440px] pr-2">
+                  {highlight}
+                </p>
+              </React.Fragment>
+            ))}
           </div>
         </div>
       ) : (
