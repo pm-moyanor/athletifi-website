e7240b181aa7f61a88e4a1cbfc52fe58e4f64dd6
Louis <louis.code88@gmail.com>
2024-03-26 23:09:26 -0600

    fix: add basic handling for user and pw updates

--- src/components/user-portal/AccountDetails.tsx
+++ src/components/user-portal/AccountDetails.tsx
@@ -1,15 +1,32 @@
+'use client';
+
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+const dummyDataUser = {
+  email: 'nameofuser@gmail.com',
+  password: '**********',
+};
+
 export default function AccountDetails() {
+  const [userData, setUserData] = useState(dummyDataUser);
+
+  function handleUserChange() {}
+
+  function handlePassChange() {}
+
   return (
     <div className="flex flex-col mt-16 text-white">
       <h2 className="rounded bg-cardsDark text-settingsGray py-2 px-2 md:px-4">
         Account Details
       </h2>
       <div className="flex justify-between items-center py-4 mx-2 md:mx-4 mt-4">
-        <div>nameofuser@gmail.com</div>
-        <div className="flex items-center cursor-pointer">
+        <div>{userData.email}</div>
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={handleUserChange}
+        >
           <div className="mx-2 md:mx-4">remove</div>
           <FontAwesomeIcon
             className="text-skyblue text-md md:text-2xl"
@@ -19,7 +36,10 @@ export default function AccountDetails() {
       </div>
       <div className="flex justify-between items-center py-4 mx-2 md:mx-4 border-t border-t-offwhite">
         <div>password: **********</div>
-        <div className="flex items-center cursor-pointer">
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={handlePassChange}
+        >
           <div className="mx-2 md:mx-4">remove</div>
           <FontAwesomeIcon
             className="text-skyblue text-md md:text-2xl"
