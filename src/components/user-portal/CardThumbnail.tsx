cdc0146dabb566ed49202cc1ec302deac8249e44
Hector Agudelo <haacny86@gmail.com>
2024-03-30 17:37:55 -0400

    toggle functionality

--- src/components/user-portal/CardThumbnail.tsx
+++ src/components/user-portal/CardThumbnail.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Card from './../../../public/assets/img/png/jose-card-img.png';
 import Image from 'next/image';
 
 const CardThumbnail: React.FC = () => {
+  const [isToggle, setIsToggle] = useState<boolean>(false);
+  const toggleEmailInput = () => {
+    setIsToggle(!isToggle);
+  };
   return (
-    <div className=" container flex flex-col bg-cardsBackground max-w-400 rounded-lg">
-      <div className="flex justify-around gap-0 mx-4">
-        <div className="flex flex-col justify-start h-full mt-1">
-          <h2 className="font-SourceSansPro font-bold text-lg md:text-lgl text-primary relative mb-1 min-w-[256px]">
+    <div className=" container flex flex-col bg-cardsBackground max-w-500 rounded-lg gap-6">
+      <div className="flex justify-around gap-0 mx-4 items-stretch">
+        <div className="flex flex-col justify-center">
+          <h2 className="font-SourceSansPro mb-2 font-bold text-lg md:text-lgl text-primary relative min-w-[256px]">
             Salvador Carillo
           </h2>
           <p className="font-SourceSansPro-Semibold text-sm md:text-base leading-6 text-start text-primary opacity-80 lg:max-w-769 relative min-w-[256px]">
@@ -20,14 +25,26 @@ const CardThumbnail: React.FC = () => {
             #22
           </p>
         </div>
-        <div className="-mt-5">
-          <Image src={Card} alt="Card Thumbnail" width={180} height={180} />
+        <div className="-mt-10">
+          <Image src={Card} alt="Card Thumbnail" width={200} height={200} />
         </div>
       </div>
-      <div className="flex justify-center">
-        <button>go to dashboard</button>
-        <button>refer to friend/family</button>
+      <div className="flex justify-center w-full border-t border-card_border">
+        <button
+          className={`text-primary font-SourceSansPro-Semibold text-sm md:text-base leading-6 w-full p-3 ${isToggle ? 'bg-buttonCardBg' : 'border-r border-card_border'}`}
+        >
+          go to dashboard
+        </button>
+        <button
+          onClick={toggleEmailInput}
+          className="text-primary font-SourceSansPro-Semibold text-sm md:text-base leading-6 w-full p-3"
+        >
+          refer to friend/family
+        </button>
       </div>
+      {isToggle && (
+        <div className="w-full p-3 bd-white">This is the hidden content</div>
+      )}
     </div>
   );
 };
