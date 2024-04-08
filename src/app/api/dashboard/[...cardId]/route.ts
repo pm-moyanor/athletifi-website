000dee4d75b08af466add2799e9b673596de7c11
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-04-08 17:41:14 -0400

    debug: added console log statements to debug internal server error with dashboard api call

--- src/app/api/dashboard/[...cardId]/route.ts
+++ src/app/api/dashboard/[...cardId]/route.ts
@@ -9,16 +9,22 @@ export async function GET(
     ? params.cardId.join('/')
     : params.cardId;
   const dashboardEndpoint = `dashboardData?dashboardSlug=${cardId}`;
+
+  console.log('Received cardId:', cardId);
+  console.log('Dashboard endpoint:', dashboardEndpoint);
+
   try {
+    console.log('Making API request...');
     const response = await axiosClient.get(dashboardEndpoint, {
       timeout: 10000,
     });
 
     const data = response.data;
+    console.log('API response data:', data);
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching dashboard data:', error);
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 },
