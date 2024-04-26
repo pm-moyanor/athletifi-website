7e4de21f060621ec05409da969ca658ad4c7a08b
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-04-25 23:19:20 -0400

    login form style overwrites

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
 import AuthClient from '@/components/auth/AuthClient';
 
 const LoginPage = () => {
-  return <AuthClient />;
+  return (
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="">
+        <AuthClient />;
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/components/auth/AuthClient.tsx
+++ src/components/auth/AuthClient.tsx
@@ -1,10 +1,46 @@
 'use client';
-
-import { Authenticator } from '@aws-amplify/ui-react';
+import 'src/styles/AuthStyles.css';
+import { Authenticator, ThemeProvider, useTheme } from '@aws-amplify/ui-react';
 import { ComponentOverrides, FormFieldsOverrides } from './AuthOverrides';
 import { signUp, type SignUpInput } from 'aws-amplify/auth';
+import { Source_Sans_3 } from 'next/font/google';
+
+const sourceSans3 = Source_Sans_3({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const loginTheme = {
+  name: 'login-theme',
+  tokens: {
+    colors: {
+      background: { primary: '#032436' },
+      border: {
+        primary: '#2B4A5C',
+        secondary: '#2B4A5C',
+      },
+      font: {
+        primary: '#FDFEFF',
+        hover: '#FDFEFF',
+        secondary: '#B1B5B8',
+      },
+    },
+
+    components: {
+      input: {},
+      authenticator: {
+        router: { borderWidth: '0', boxShadow: 'none' },
+        form: { padding: '0px' },
+        field: {},
+      },
+    },
+  },
+};
 
 const AuthClient = () => {
+  const { tokens } = useTheme();
+  console.log(tokens);
+
   const baseURL =
     process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/api';
   const postHelper = async (
@@ -29,11 +65,24 @@ const AuthClient = () => {
   };
 
   return (
-    <Authenticator
-      components={ComponentOverrides}
-      formFields={FormFieldsOverrides}
-      services={services}
-    />
+    <div className={`bg-cardsDark ${sourceSans3.className} rounded-10 p-8 `}>
+      <ThemeProvider theme={loginTheme}>
+        <div className="mb-12">
+          <p className="text-primary font-extralight leading-8">Welcome to</p>
+          <h1
+            className={`text-xl text-primary leading-8 ${sourceSans3.className}`}
+          >
+            Athletifi
+          </h1>
+        </div>
+
+        <Authenticator
+          components={ComponentOverrides}
+          formFields={FormFieldsOverrides}
+          services={services}
+        />
+      </ThemeProvider>
+    </div>
   );
 };
 
