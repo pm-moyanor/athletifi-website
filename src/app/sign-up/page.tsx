8588b9b4a2d5a94c4bbd3e20364d97f877a830ff
Louis <louis.code88@gmail.com>
2024-05-15 13:35:24 -0600

    fix: update SEO metadata to follow nextjs14 convention

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -4,11 +4,13 @@ import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import SignUpForm from '@/components/sign-up/SignUpForm';
 import { SEO_CONFIG } from '@/utils/seoConfig';
+import { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL(SEO_CONFIG.signUp.websiteURL),
   title: SEO_CONFIG.signUp.title,
-  description: SEO_CONFIG.signUp.description,
   openGraph: {
+    description: SEO_CONFIG.signUp.description,
     images: SEO_CONFIG.signUp.image,
   },
 };
