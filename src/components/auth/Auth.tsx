98ebe5ff7a940373ec27a653b14df94d1898be36
Quentin Vidal <33239211+qisforq@users.noreply.github.com>
2024-05-29 23:51:59 -0400

    feat(debug): add console.log for troubleshooting redirect url issue

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 // import config from '@/amplifyconfiguration.json';
-// import awsExports from '@/aws-exports';
+import awsExports from '@/aws-exports';
 import config from '@/custom-aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 import { Authenticator } from '@aws-amplify/ui-react';
@@ -12,6 +12,11 @@ Amplify.configure(config, { ssr: true });
 // Amplify.configure(awsExports, { ssr: true });
 
 const Auth = ({ children }: { children: React.ReactNode }) => {
+  console.log(
+    'custom redirectSignIn URL (Auth.tsx):',
+    config.oauth.redirectSignIn,
+  );
+  console.log('original redirectSignIn URL:', awsExports.oauth.redirectSignOut);
   return <Authenticator.Provider>{children}</Authenticator.Provider>;
 };
 
