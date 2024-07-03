fe3b8efa2989e2029291fb8f3fb6585659c3acdf
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-07-03 10:42:45 -0400

    add conditional to profilecard

--- src/components/dashboard/ProfileCard.tsx
+++ src/components/dashboard/ProfileCard.tsx
@@ -10,49 +10,69 @@ const Profile: React.FC = () => {
   const profile = dashboardData.data?.playerProfile;
   return (
     <>
-      {profile?.age ? (
-        <div className="w-full h-[560px] bg-cardsBackground rounded-10 p-4 mb-4 mt-4  lg:mb-0 lg:-mt-[100px] flex flex-col tracking-wide">
+      {profile ? (
+        <div className="w-full h-[560px] bg-cardsBackground rounded-10 p-4 my-4 lg:my-0 lg:-mt-[100px] flex flex-col tracking-wide">
           <h2 className="text-primary text-[24px] mb-4 font-semibold">
             Profile
           </h2>
           <table className="text-primary w-full divide-y divide-gray-200">
             <tbody className="font-light text-base">
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">Age</td>
-                <td className="text-right min-w-24 px-[4px]">{profile?.age}</td>
-              </tr>
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">Club</td>
-                <td className="text-right px-[4px]">{profile?.club}</td>
-              </tr>
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">League</td>
-                <td className="text-right px-[4px]">{profile?.league}</td>
-              </tr>
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">Team name</td>
-                <td className="text-right px-[4px]">{profile?.team}</td>
-              </tr>
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">Age group</td>
-                <td className="text-right px-[4px]">{profile?.age_group}</td>
-              </tr>
-              <tr className="border-b border-partnersBorders">
-                <td className="text-left py-2 px-[4px]">Gender</td>
-                <td className="text-right px-[4px]">{profile?.gender}</td>
-              </tr>
-              <tr>
-                <td className="text-left py-2 px-[4px]">Coach</td>
-                <td className="text-right px-[4px]">{profile?.coach}</td>
-              </tr>
+              {profile.age && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">Age</td>
+                  <td className="text-right min-w-24 px-[4px]">
+                    {profile.age}
+                  </td>
+                </tr>
+              )}
+              {profile.club && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">Club</td>
+                  <td className="text-right px-[4px]">{profile.club}</td>
+                </tr>
+              )}
+              {profile.league && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">League</td>
+                  <td className="text-right px-[4px]">{profile.league}</td>
+                </tr>
+              )}
+              {profile.team && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">Team name</td>
+                  <td className="text-right px-[4px]">{profile.team}</td>
+                </tr>
+              )}
+              {profile.age_group && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">Age group</td>
+                  <td className="text-right px-[4px]">{profile.age_group}</td>
+                </tr>
+              )}
+              {profile.gender && (
+                <tr className="border-b border-partnersBorders">
+                  <td className="text-left py-2 px-[4px]">Gender</td>
+                  <td className="text-right px-[4px]">{profile.gender}</td>
+                </tr>
+              )}
+              {profile.coach && (
+                <tr>
+                  <td className="text-left py-2 px-[4px]">Coach</td>
+                  <td className="text-right px-[4px]">{profile.coach}</td>
+                </tr>
+              )}
             </tbody>
           </table>
-          <h3 className="text-primary text-[20px] mt-6 mb-2 w-full">Bio</h3>
-          <div className="h-full overflow-y-auto mb-4 ">
-            <p className="text-primary font-light text-base pl-[4px] pr-2">
-              {profile?.bio}
-            </p>
-          </div>
+          {profile.bio && (
+            <>
+              <h3 className="text-primary text-[20px] mt-6 mb-2 w-full">Bio</h3>
+              <div className="h-full overflow-y-auto mb-4">
+                <p className="text-primary font-light text-base">
+                  {profile.bio}
+                </p>
+              </div>
+            </>
+          )}
         </div>
       ) : (
         <div className="lg:-mt-[86px]">
