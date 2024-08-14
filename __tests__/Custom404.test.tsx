7c3ed846467d14dc00d7d119e10c50e4ab14381c
Louis <louis.code88@gmail.com>
2024-08-13 22:23:58 -0600

    fix: update naming

--- __tests__/Custom404.test.tsx
+++ __tests__/Custom404.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Custom404 from '@/components/common/Custom404';
 
-describe('ContactForm', () => {
+describe('Custom404', () => {
   it('renders a heading', () => {
     render(<Custom404 />); // ARRANGE
 
