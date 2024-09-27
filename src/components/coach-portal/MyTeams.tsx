8beada0495c6d7425f603f246920127ce0162818
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2024-09-27 12:14:16 -0400

    adjust margins

--- src/components/coach-portal/MyTeams.tsx
+++ src/components/coach-portal/MyTeams.tsx
@@ -2,7 +2,7 @@ import Team from './Team';
 
 const MyTeams: React.FC = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-12 mx-auto max-w-[1040px]">
       <h1 className="text-[32px] text-primary font-bold mb-4">My Teams</h1>
       <p className="text-base font-light max-w-[480px] text-primary">
         Some copy to explain wht you find here....
