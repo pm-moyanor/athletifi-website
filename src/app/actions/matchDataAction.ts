92622ee02f8f6b2a2feb1cdb7d938305b047f8e0
Louis <louis.code88@gmail.com>
2024-09-30 12:35:07 -0600

    feat: add basic match data upload server action (needs additional content)

--- src/app/actions/matchDataAction.ts
+++ src/app/actions/matchDataAction.ts
@@ -7,13 +7,76 @@ import { cookies } from 'next/headers';
 const matchDataUploadUrl = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/matchDataUpload`;
 const videoUploadUrl = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/videoUpload`;
 
-interface UploadResult {
+interface Player {
+  id: string;
+  name: string;
+  number: string;
+  notes: string;
+  status: string;
+}
+
+interface MatchDataUpload {
+  date: string;
+  location: string;
+  main_team: string;
+  opp_team: string;
+  main_team_side: string;
+  roster: Player[];
+  main_jersey_color: string;
+  opp_jersey_color: string;
+  images: string;
+}
+
+interface UploadResponse {
+  message?: string | null;
+  error?: string | null;
+}
+
+interface VideoUploadResult {
   success: boolean;
   fileKey?: string;
   error?: string;
 }
 
-export async function uploadVideo(formData: FormData): Promise<UploadResult> {
+async function uploadMatchDataHelper(
+  matchDataUpload: MatchDataUpload,
+): Promise<UploadResponse> {
+  const response = await fetch(`${matchDataUploadUrl}`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: process.env.NEXT_PUBLIC_TEMP_API_AUTH,
+    } as HeadersInit,
+    body: JSON.stringify(matchDataUpload),
+  });
+
+  const data: UploadResponse = await response.json();
+  return data;
+}
+
+export async function uploadMatchData(formData: FormData) {
+  const matchUploadData: MatchDataUpload = {};
+
+  try {
+    const response = await uploadMatchDataHelper(matchUploadData);
+
+    // Revalidate the page to reflect the new upload
+    revalidatePath('/coach-portal');
+
+    return { success: true };
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    return {
+      success: false,
+      error:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+    };
+  }
+}
+
+export async function uploadVideo(
+  formData: FormData,
+): Promise<VideoUploadResult> {
   const file = formData.get('file') as File | null;
   if (!file) {
     throw new Error('No file provided');
